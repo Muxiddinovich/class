35670607370cefc6be47f751581ffcb2c6b1c769
Sherbek <sherbekkubayev@gmail.com>
2025-01-09 11:46:10 +0500

    class

---
+++ main.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, first_name, last_name, year):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.year=year
+
+person1=Person("Sherbek", "Kubayev", 2008)
+
+a=5
+
+print(type(Person))
+
+
