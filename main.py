112ac9e1e619e29f0ec328427a2a5975be6b8e4d
Sherbek <sherbekkubayev@gmail.com>
2025-01-09 11:54:53 +0500

    get_info qoshildi

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+# class Person:
+#     def __init__(self, first_name, last_name, year):
+#         self.first_name=first_name
+#         self.last_name=last_name
+#         self.year=year
+
+# person1=Person("Sherbek", "Kubayev", 2008)
+
+# a=5
+
+# print(type(Person))
+
+
+
+
 class Person:
     def __init__(self, first_name, last_name, year):
         self.first_name=first_name
         self.last_name=last_name
         self.year=year
 
+    def get_info(self):
+        print(f"Mening ismim {self.first_name}!")
+
+
 person1=Person("Sherbek", "Kubayev", 2008)
 
-a=5
-
-print(type(Person))
-
-
+person1.get_info()
