9ad96bc583ecee678e86c4e5bcbae7bde12c1b8f
Sherbek <sherbekkubayev@gmail.com>
2025-01-12 19:47:28 +0500

    PupilManager

--- pupil.py
+++ pupil.py
@@ -20,5 +20,53 @@ class PupilManager:
 
     def add_pupil(self, pupil):
         self.pupils.append(pupil)
+    
+
+    def get_pupils(self):
+        for pupil in self.pupils:
+            print(pupil.get_info())
+
+
+
+    def get_max_gpa(self):
+        gpa=0
+        for pupil in self.pupils:
+            if pupil.gpa > gpa:
+                gpa = pupil.gpa
+
+        return gpa
+    
+
+
+    def change_gpa(self, ism, fam, new_gpa):
+        for pupil in self.pupils:
+            if pupil.ism==ism and pupil.fam==fam:
+                pupil.gpa = new_gpa
+
+
+
+
+
+
+
+pupil1=Pupil("Sherbek", "Kubayev", 2008, 100)
+pupil2=Pupil("Sherbek2", "Kubayev2", 2008, 90)
+
+
+manager=PupilManager()
+manager2=PupilManager()
+
+manager.add_pupil(pupil1)
+manager.add_pupil(pupil2)
+
+manager.change_gpa("Sherbek2", "Kubayev2", 110)
+
+
+
+
+
+
+manager.get_pupils()
+
 
 
